8894a41387f1b23202b3aba83a2e92ee7462c83b
filipciesielski7 <filipcies@gmail.com>
2022-01-14 00:09:49 +0100

    Add header

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,1 +1,3 @@
 export const HOME = "/";
+export const GITHUB = "https://github.com/filipciesielski7";
+export const LINKEDIN = "https://www.linkedin.com/in/filip-ciesielski-45aa4320b/";
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,32 @@
 import React from "react";
+import Header from "../components/header";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        className="bg-dark"
+      >
+        <h2 style={{ color: "white", fontWeight: 300 }}>Filip Ciesielski</h2>
+      </div>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        className="bg-light"
+      >
+        <h2 style={{ color: "black", fontWeight: 300 }}>Filip Ciesielski</h2>
+      </div>
+    </>
+  );
 }
