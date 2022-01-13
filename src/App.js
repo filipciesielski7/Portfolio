8e9b8306040ba439cb169576ad205e60e330cd69
filipciesielski7 <filipcies@gmail.com>
2022-01-13 20:12:14 +0100

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import ScrollToTop from "./helpers/scrollToTop";
 
-function App() {
+import Home from "./pages/home";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <Switch>
+        <Route path={ROUTES.HOME}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
